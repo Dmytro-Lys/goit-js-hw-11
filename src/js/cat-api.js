6ef35efe0c6fa78e1550580ff595628ff5f0c6df
Dmytro Lysenko <121360853+Dmytro-Lys@users.noreply.github.com>
2023-06-12 10:48:16 +0300

    Initial commit

---
+++ src/js/cat-api.js
@@ -0,0 +1,12 @@
+const API_KEY = "live_Qqf75mrVJLuN2NBOiCh8k6zoQf7XNpaOEFGjJNI2zFtDyHvDOLeGLIHUl9rrirK2"
+
+function fetchBreeds() {
+  const url = `https://api.thecatapi.com/v1/breeds?api_key=${API_KEY}`;
+  return fetch(url).then((res) => res.json());
+}
+
+function fetchCatByBreed(breedId){
+  const url = `https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}&breed_ids=${breedId}`;
+  return fetch(url).then((res) => res.json());
+}
+export { fetchBreeds, fetchCatByBreed };
