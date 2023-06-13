706c1cbee2452890446d8878440e412c589aa87d
Dmytro-Lys <d.lysenko122@gmail.com>
2023-06-13 08:58:08 +0300

    base changes

--- src/index.js
+++ src/index.js
@@ -1,105 +1,98 @@
-import { fetchBreeds, fetchCatByBreed } from "./js/cat-api.js";
+import { getImages } from "./js/px-api.js";
 import Notiflix from 'notiflix';
 import "notiflix/src/notiflix.css";
-import SlimSelect from 'slim-select';
-import "slim-select/dist/slimselect.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-Notiflix.Notify.init({
-    position: 'center-top',
-    distance: '40px',
-    timeout: 3600000
-})
-
-let eventError = false;
-
+let page = 1;
+let querry = "";
+let cardHeight = 288;
 const refs = {
-    select: document.querySelector(".breed-select"),
-    divData: document.querySelector(".cat-info")
+    form: document.querySelector('#search-form'),
+    gallery: document.querySelector('.gallery'),
+    btnLoadMore: document.querySelector('.load-more')
 }
+const gallerySLb = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: "250" });
 
-startLoading(refs.select);
-fetchBreeds().then((data) => {
-    if (!data.length) throw new Error("Data not found");
-    return data.reduce(
-        (markup, currentEl) => markup + createSelectElement(currentEl), "");
-}).then(updateSelect).catch(onError)
-    .finally(endLoading);
-
-refs.select.addEventListener('change', onSelect);
-
-// functions
-function createSelectElement({id, name}) {
-    return `<option value="${id}">${name ||"Unknown"}</option>`
-}
-
-function updateSelect(markup) {
-    refs.select.innerHTML = markup;
-    new SlimSelect({
-        select: refs.select
+window.scrollBy({
+        top: cardHeight * 2,
+        behavior: "smooth"
     });
-    refs.select.classList.remove("invisible");
+refs.form.addEventListener("submit", onSubmit);
+refs.btnLoadMore.addEventListener("click", fetchImages);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const inputValue = refs.form.elements.searchQuery.value.trim();
+
+    if (inputValue === "") return Notiflix.Notify.failure("Empty query!");
+    querry = inputValue;
+    clearImgList();
+    refs.btnLoadMore.classList.remove("invisible");
+  page = 1;
+  
+  fetchImages()
+    .catch(onError)
+    .finally(() => refs.form.reset());
+}
     
+
+async function fetchImages() {
+
+    refs.btnLoadMore.disable = true;
+  try {
+     const data = await getImages(querry, page);
+      if (!data.hits.length) throw new Error("Data not found");
+      page += 1;
+      const maxPage = Math.ceil(data.totalHits / 40);
+      if (page > maxPage) refs.btnLoadMore.classList.add("invisible");
+     const markup = await data.hits.reduce(
+            (markup, currentEl) => markup + createGalleryItem(currentEl), "");
+        if (markup === undefined) throw new Error("No data!");
+      refs.gallery.insertAdjacentHTML("beforeend", markup);
+      gallerySLb.refresh();
+    //   const { height: cardHeight } = refs.gallery.firstElementChild.getBoundingClientRect();
+     
+  } catch (err) {
+    onError(err);
+  }
+
+refs.btnLoadMore.disable = false ;
+
 }
 
-function onSelect(e) {
-    startLoading(refs.divData);
-    fetchCatByBreed(e.target.value).then((data) => {
-       if (!data.length) throw new Error("Data not found"); 
-       return data.reduce(
-        (markup, currentEl) => markup + createInfoElement(getArgs(currentEl)), "");
-    }).then(updateInfo)
-        .catch(onError)
-        .finally(endLoading);  
-}
 
 
-function createInfoElement({url,  name, description, temperament}) {    
-   return ` <img
-      class="cat_image"
-      src="${url}"
-      alt="${name ||"Unknown"}"
+function createGalleryItem({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) {
+    return `<div class="photo-card">
+  <a class="gallery__link" href="${largeImageURL}">
+    <img
+      class="gallery__image"
+      src="${webformatURL}"
+      alt="${tags}"
     />
-    <div class="cat-info-text">
-    <h2>${name ||"Unknown"}</h2>
-    <p>${description ||"Unknown"}</p>
-    <p><b>Temperament: </b>${temperament ||"Unknown"}</p>
-    </div>`
-}
+    <div class="info">
+    <p class="info-item">
+      <b>Likes</b> ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b> ${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b> ${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b> ${downloads}
+    </p>
+    </div>
+  </a>
+</div>`;
+};
 
-function updateInfo(markup) {
-    refs.divData.innerHTML = markup;
-    refs.divData.classList.remove("invisible");
-}
-
-function getArgs({ url, breeds }) {
-    const { name, description, temperament } = breeds[0];
-    return {
-        url,
-        name,
-        description,
-        temperament
-    }
-}
-
-function startLoading(element) {
-    if (eventError) afterError();
-    element.classList.add("invisible");
-    Notiflix.Loading.hourglass('Loading data, please wait...',{
-  backgroundColor: 'rgba(0,0,0,0.6)',
-});
-}
-
-function endLoading() {
-    Notiflix.Loading.remove();
+function clearImgList() {
+  refs.gallery.innerHTML = "";
 }
 
 function onError(error) {
-    eventError = true;
     Notiflix.Notify.failure(error.message);
-}
-
-function afterError() {
-    const notify = document.querySelector(".notiflix-notify-failure");
-    if (notify) notify.remove();
-    eventError = false;
 }
